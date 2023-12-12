9a83105d7e506a91cbfc2aa18071af7c12afed98
ArmenSadyhyan <150845415+ArmenSadyhyan@users.noreply.github.com>
2023-12-12 14:45:50 +0300

    2 камеры, скрипт
    Написал скрипт по смене камеры, добавил вторую камеру

--- BallRun 1.0/Assets/scripts/CameraTrigger.cs
+++ BallRun 1.0/Assets/scripts/CameraTrigger.cs
@@ -1,18 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject camera1;
+    public GameObject camera2;
+
+    
+
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.tag == "TriggerLVL")
+        {
+            camera1.SetActive(false);
+            camera2.SetActive(true);
+        }
+
+
+
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
